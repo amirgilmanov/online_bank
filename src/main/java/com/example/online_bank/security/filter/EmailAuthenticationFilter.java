3083223821e8720117270b27a3845ce7085ae51d
agilmanov <79984458+amirgilmanov@users.noreply.github.com>
2025-09-04 22:42:29 +0500

    Update EmailAuthenticationFilter.java

--- src/main/java/com/example/online_bank/security/filter/EmailAuthenticationFilter.java
+++ src/main/java/com/example/online_bank/security/filter/EmailAuthenticationFilter.java
@@ -1,1 +1,75 @@
-package com.example.online_bank.security.filter;import com.example.online_bank.security.token.EmailAuthenticationToken;import com.fasterxml.jackson.databind.JsonNode;import com.fasterxml.jackson.databind.ObjectMapper;import jakarta.servlet.FilterChain;import jakarta.servlet.ServletException;import jakarta.servlet.http.HttpServletRequest;import jakarta.servlet.http.HttpServletResponse;import lombok.RequiredArgsConstructor;import lombok.extern.slf4j.Slf4j;import org.springframework.security.authentication.AuthenticationManager;import org.springframework.security.core.Authentication;import org.springframework.security.core.context.SecurityContextHolder;import org.springframework.stereotype.Component;import org.springframework.web.filter.OncePerRequestFilter;import java.io.BufferedReader;import java.io.IOException;@Component@Slf4j@RequiredArgsConstructorpublic class EmailAuthenticationFilter extends OncePerRequestFilter {    private final AuthenticationManager authenticationManager;    private static final String AUTH_PATH_PREFIX = "/api/authentication";    /**     * @param request     * @param response     * @param filterChain     * @throws ServletException     * @throws IOException     */    @Override    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {        String requestBodyStr = readBody(request);        log.debug("Парсим и читаем json");        ObjectMapper objectMapper = new ObjectMapper();        JsonNode jsonNode = objectMapper.readTree(requestBodyStr);        String email = jsonNode.get("email").asText();        String code = jsonNode.get("code").asText();        EmailAuthenticationToken authToken = new EmailAuthenticationToken(email, code);        Authentication authResult = this.authenticationManager.authenticate(authToken);        if (SecurityContextHolder.getContext().getAuthentication() == null) {            SecurityContextHolder.getContext().setAuthentication(authResult);        }        filterChain.doFilter(request, response);    }    /**     * True = "Да, НЕ фильтруй" → пропускаем     * false = "Нет, фильтруй" → выполняем фильтр     *     * @param request     * @return     * @throws ServletException     */    @Override    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {        String requestPath = request.getRequestURI();        return !requestPath.startsWith(AUTH_PATH_PREFIX);    }    private String readBody(HttpServletRequest request) throws IOException {        StringBuilder sb = new StringBuilder();        log.debug("Читаем тело запроса");        try (BufferedReader reader = request.getReader()) {            String line;            while ((line = reader.readLine()) != null) {                sb.append(line);            }        }        String requestBodyStr = sb.toString();        log.info("Тело запроса - {}", requestBodyStr);        return requestBodyStr;    }}
+package com.example.online_bank.security.filter;
+
+import com.example.online_bank.security.jwt.service.impl.JwtUtilImpl;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.security.SignatureException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Date;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+    /**
+     * @param request
+     * @param response
+     * @param filterChain
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authzHeader = request.getHeader("Authorization");
+        String username = null;
+        String jwtToken = null;
+
+        String pathInfo = request.getRequestURI();
+        if (pathInfo.equals("/login")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        if (authzHeader != null && authzHeader.startsWith("Bearer ")) {
+            jwtToken = authzHeader.substring(7);
+            Date date = new Date();
+
+            // Получаем информацию о пользователе.
+            // Проверяем пользователя.
+            //Библиотека jjwt проверяет подпись, дату истечения
+            try {
+                username = jwtUtil.getUserName(jwtToken);
+            } catch (SignatureException e) {
+                log.warn("Время жизни токена вышло");
+            } catch (ExpiredJwtException e) {
+                log.warn("Токен устарел");
+            }
+        }
+
+        //Проверка на то, что контекст не занят кем-то другим
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            //TODO узнать что здесь происходит
+            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+                    username,
+                    null,
+                    jwtUtil.getUserAuthoritiesFromToken(jwtToken).stream()
+                            .map(SimpleGrantedAuthority::new)
+                            .toList()
+            );
+            SecurityContextHolder.getContext().setAuthentication(token);
+        }
+        filterChain.doFilter(request, response);
+    }
+}
