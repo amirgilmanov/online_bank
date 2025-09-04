efa974c81c56f926822eabe7aac8ac8a6bd3e999
agilmanov <79984458+amirgilmanov@users.noreply.github.com>
2025-09-05 00:57:59 +0500

    Update Role.java

--- src/main/java/com/example/online_bank/domain/entity/Role.java
+++ src/main/java/com/example/online_bank/domain/entity/Role.java
@@ -1,1 +1,21 @@
-package com.example.online_bank.domain.entity;import jakarta.persistence.*;import lombok.AllArgsConstructor;import lombok.Getter;import lombok.NoArgsConstructor;import lombok.Setter;@Entity@AllArgsConstructor@NoArgsConstructor@Getter@Setterpublic class Role {    @Id    @GeneratedValue(strategy = GenerationType.IDENTITY)    private Long id;    @Column    private String name;}
+package com.example.online_bank.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+}
