1ffd96b7eac2b02b02e173ec6dde383909ca970d
agilmanov <79984458+amirgilmanov@users.noreply.github.com>
2025-09-05 01:06:57 +0500

    Update JwtFactory.java

--- src/main/java/com/example/online_bank/security/jwt/factory/JwtFactory.java
+++ src/main/java/com/example/online_bank/security/jwt/factory/JwtFactory.java
@@ -1,1 +1,11 @@
-package com.example.online_bank.security.jwt.factory;import com.example.online_bank.enums.TokenType;import org.springframework.security.core.Authentication;/** * Этот класс выдает необходимый тип токена */public interface JwtFactory {    String createJwt(TokenType tokenType, Authentication authentication);}
+package com.example.online_bank.security.jwt.factory;
+
+import com.example.online_bank.enums.TokenType;
+import org.springframework.security.core.Authentication;
+
+/**
+ * Этот класс выдает необходимый тип токена
+ */
+public interface JwtFactory {
+    String createJwt(TokenType tokenType, Authentication authentication);
+}
