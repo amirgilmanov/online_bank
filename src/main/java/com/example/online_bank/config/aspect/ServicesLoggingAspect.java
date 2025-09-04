cc43fe814e1e2688d26cac313de4642216091f72
agilmanov <79984458+amirgilmanov@users.noreply.github.com>
2025-09-05 00:54:09 +0500

    Update ServicesLoggingAspect.java

--- src/main/java/com/example/online_bank/config/aspect/ServicesLoggingAspect.java
+++ src/main/java/com/example/online_bank/config/aspect/ServicesLoggingAspect.java
@@ -1,1 +1,26 @@
-package com.example.online_bank.config.aspect;import lombok.extern.slf4j.Slf4j;import org.aspectj.lang.JoinPoint;import org.aspectj.lang.annotation.Aspect;import org.aspectj.lang.annotation.Before;import org.aspectj.lang.annotation.Pointcut;import org.springframework.stereotype.Component;@Component@Aspect@Slf4jpublic class ServicesLoggingAspect {    @Pointcut(value = "execution(* com.example.online_bank.service.*.*(..))")    public void pointCut() {    }    @Before("pointCut()")    public void logBefore(JoinPoint joinPoint) {        String className = joinPoint.getTarget().getClass().getName();        String methodName = joinPoint.getSignature().getName();        Object[] methodArgs = joinPoint.getArgs();        log.info("Метод {} с аргументами {}",className + "." + methodName, methodArgs);    }}
+package com.example.online_bank.config.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+@Slf4j
+public class ServicesLoggingAspect {
+
+    @Pointcut(value = "execution(* com.example.online_bank.service.*.*(..))")
+    public void pointCut() {
+    }
+
+    @Before("pointCut()")
+    public void logBefore(JoinPoint joinPoint) {
+        String className = joinPoint.getTarget().getClass().getName();
+        String methodName = joinPoint.getSignature().getName();
+        Object[] methodArgs = joinPoint.getArgs();
+        log.info("Метод {} с аргументами {}",className + "." + methodName, methodArgs);
+    }
+}
