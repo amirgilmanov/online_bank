6f3885ea12026ff1f1a966d8f415afeee4fe2b76
agilmanov <79984458+amirgilmanov@users.noreply.github.com>
2025-09-05 01:03:17 +0500

    Update VerifiedCode.java

--- src/main/java/com/example/online_bank/domain/entity/VerifiedCode.java
+++ src/main/java/com/example/online_bank/domain/entity/VerifiedCode.java
@@ -1,1 +1,42 @@
-package com.example.online_bank.domain.entity;import com.example.online_bank.enums.VerifiedCodeType;import jakarta.persistence.*;import lombok.*;import java.time.LocalDateTime;import java.util.UUID;@Entity@Getter@Setter@ToString@Builder@AllArgsConstructor@NoArgsConstructorpublic class VerifiedCode {        @Id        @GeneratedValue(strategy = GenerationType.UUID)        private UUID id;        @Column        private String verifiedCode;        @Column        private LocalDateTime createdAt;        @Column        private LocalDateTime expiresAt;        @Column        private Boolean isVerified;        @Column        @Enumerated(EnumType.STRING)        private VerifiedCodeType codeType;        @ManyToOne()        @JoinColumn(name = "user_id", referencedColumnName = "id")        @ToString.Exclude        private User user;}
+package com.example.online_bank.domain.entity;
+
+import com.example.online_bank.enums.VerifiedCodeType;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class VerifiedCode {
+        @Id
+        @GeneratedValue(strategy = GenerationType.UUID)
+        private UUID id;
+
+        @Column
+        private String verifiedCode;
+
+        @Column
+        private LocalDateTime createdAt;
+
+        @Column
+        private LocalDateTime expiresAt;
+
+        @Column
+        private Boolean isVerified;
+
+        @Column
+        @Enumerated(EnumType.STRING)
+        private VerifiedCodeType codeType;
+
+        @ManyToOne()
+        @JoinColumn(name = "user_id", referencedColumnName = "id")
+        @ToString.Exclude
+        private User user;
+}
