8d2f10339f44d16ae6ca3ea26f076c5a91190233
agilmanov <79984458+amirgilmanov@users.noreply.github.com>
2025-09-05 01:07:19 +0500

    Update TokenFactory.java

--- src/main/java/com/example/online_bank/security/jwt/factory/TokenFactory.java
+++ src/main/java/com/example/online_bank/security/jwt/factory/TokenFactory.java
@@ -1,1 +1,10 @@
-package com.example.online_bank.security.jwt.factory;import com.example.online_bank.enums.TokenType;import org.springframework.security.core.Authentication;public interface TokenFactory {    String createToken(TokenType type, Authentication auth);    boolean supports(TokenType supported);}
+package com.example.online_bank.security.jwt.factory;
+
+import com.example.online_bank.enums.TokenType;
+import org.springframework.security.core.Authentication;
+
+public interface TokenFactory {
+    String createToken(TokenType type, Authentication auth);
+
+    boolean supports(TokenType supported);
+}
