08f5b8fddd4fb26bca1d9938f069c348c40f3df9
gilmi19 <gilmanovamir19@gmail.com>
2026-02-21 12:25:51 +0500

    Костыльные изменения изза ограниченного времени и функционала

--- src/main/java/com/example/online_bank/service/FillDb.java
+++ src/main/java/com/example/online_bank/service/FillDb.java
@@ -1,9 +1,12 @@
 package com.example.online_bank.service;
 
+import com.example.online_bank.domain.dto.RegistrationDto;
 import com.example.online_bank.domain.entity.User;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
@@ -11,10 +14,15 @@ public class FillDb {
     private final QuestService questService;
     private final UserQuestService userQuestService;
     private final UserService userService;
+    private final RegistrationService registrationService;
 
     @PostConstruct
     private void fillQuest(){
         questService.createRandomQuest();
+        Optional<User> byEmail = userService.findByEmail("gilmanovamir19@gmail.com");
+        if (byEmail.isEmpty()){
+            registrationService.signUp(new RegistrationDto("a", "a", "a", "a", "1234", "gilmanovamir19@gmail.com"));
+        }
         User user = userService.findByEmail("gilmanovamir19@gmail.com").orElseThrow(RuntimeException::new);
         userQuestService.makeRelationBetweenUserAndQuest(user);
     }
